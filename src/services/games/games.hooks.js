44a82426cb8d4fa29867d828da712c548cad37f9
Haico27 <haico.wensink@gmail.com>
2017-06-25 23:13:20 +0200

    add authentication before hooks to games service

--- src/services/games/games.hooks.js
+++ src/services/games/games.hooks.js
@@ -1,14 +1,32 @@
+const { authenticate } = require('feathers-authentication').hooks;
+const { restrictToOwner, associateCurrentUser, restrictToAuthenticated } = require('feathers-authentication-hooks');
 
+const restrict = [
+  authenticate('jwt'),
+  restrictToAuthenticated(),
+  restrictToOwner({
+    ownerField: 'authorId'
+  })
+];
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    create: [
+      authenticate('jwt'),
+      associateCurrentUser({ as: 'authorId' }), //this will add the _id of the current user to the games authorId
+    ],
+    update: [
+      ...restrict
+    ],
+    patch: [
+      ...restrict
+    ],
+    remove: [
+      ...restrict
+    ]
   },
 
   after: {
