a7927bec1b8c883688c41e1cb3a21b4cb4aedbcb
Haico27 <haico.wensink@gmail.com>
2017-06-25 23:03:55 +0200

    generate authentication

--- src/models/games.model.js
+++ src/models/games.model.js
@@ -6,16 +6,16 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
 
 
-  const playerShema = new mongooseCient.Schema({
-    userId: { type: Schema.Types.ObjectId, ref: 'users' },
+  const playerSchema = new mongooseClient.Schema({
+    userId: { type: mongooseClient.Schema.Types.ObjectId, ref: 'users' },
     score: { type: Number, default: 0 } //player starts with a score of 0.
   })
 
 
   const games = new mongooseClient.Schema({
     players: [playerSchema],
-    winnerId: { type: Schema.Types.ObjectId, ref: 'users' },
-    authorId: { type: Schema.Types.ObjectId, ref: 'users'}
+    winnerId: { type: mongooseClient.Schema.Types.ObjectId, ref: 'users' },
+    authorId: { type: mongooseClient.Schema.Types.ObjectId, ref: 'users'},
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
