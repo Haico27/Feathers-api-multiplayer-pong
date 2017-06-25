d2ee29d5e7a886d88b3fc924d32aeb69bdf9e891
Haico27 <haico.wensink@gmail.com>
2017-06-25 22:58:38 +0200

    create first game model

--- src/models/games.model.js
+++ src/models/games.model.js
@@ -1,11 +1,21 @@
 // games-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
+
+
+  const playerShema = new mongooseCient.Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'users' },
+    score: { type: Number, default: 0 } //player starts with a score of 0.
+  })
+
+
   const games = new mongooseClient.Schema({
-    text: { type: String, required: true },
+    players: [playerSchema],
+    winnerId: { type: Schema.Types.ObjectId, ref: 'users' },
+    authorId: { type: Schema.Types.ObjectId, ref: 'users'}
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
